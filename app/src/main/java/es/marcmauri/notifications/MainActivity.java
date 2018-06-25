433f6f605345d2dc3c10102765eb47a7be7e8002
Marc from GitKraken <marcmauri92@gmail.com>
2018-06-25 23:17:07 +0200

    Se implemento el metodo sendNotification() para poder crear y  mostrar las notificaciones

--- app/src/main/java/es/marcmauri/notifications/MainActivity.java
+++ app/src/main/java/es/marcmauri/notifications/MainActivity.java
@@ -1,13 +1,16 @@
 package es.marcmauri.notifications;
 
+import android.app.Notification;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnCheckedChanged;
@@ -19,7 +22,11 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.editTextMessage) EditText editTextMessage;
     @BindView(R.id.switchImportance) Switch switchImportance;
 
+    @BindString(R.string.switch_notifications_on) String switchTextOn;
+    @BindString(R.string.switch_notifications_off) String switchTextOff;
+
     private boolean isHighImportance = false;
+    private NotificationHandler notificationHandler;
 
 
     @Override
@@ -27,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this); // Right after setContentView
+        notificationHandler = new NotificationHandler(this);
     }
 
     @OnClick(R.id.buttonSend)
@@ -37,9 +45,16 @@ public class MainActivity extends AppCompatActivity {
     @OnCheckedChanged(R.id.switchImportance)
     public void change(CompoundButton compoundButton, boolean isChecked) {
         isHighImportance = isChecked;
+        switchImportance.setText((isChecked) ? switchTextOn : switchTextOff);
     }
 
     private void sendNotification() {
-        //
+        String title = editTextTitle.getText().toString();
+        String message = editTextMessage.getText().toString();
+
+        if (!TextUtils.isEmpty(title) && !TextUtils.isEmpty(message)) {
+            Notification.Builder nb = notificationHandler.createNotification(title, message, isHighImportance);
+            notificationHandler.getManager().notify(1, nb.build());
+        }
     }
 }
