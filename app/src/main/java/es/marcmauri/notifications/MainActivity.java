987d2949509e9a5af9bc6cde612d07f6baa97b3d
Marc from GitKraken <marcmauri92@gmail.com>
2018-06-25 17:39:13 +0200

    Implementacion de binder usando Butter Knife

--- app/src/main/java/es/marcmauri/notifications/MainActivity.java
+++ app/src/main/java/es/marcmauri/notifications/MainActivity.java
@@ -2,12 +2,44 @@ package es.marcmauri.notifications;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.editTextTitle) EditText editTextTitle;
+    @BindView(R.id.editTextMessage) EditText editTextMessage;
+    @BindView(R.id.switchImportance) Switch switchImportance;
+
+    private boolean isHighImportance = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this); // Right after setContentView
+    }
+
+    @OnClick(R.id.buttonSend)
+    public void click() {
+        sendNotification();
+    }
+
+    @OnCheckedChanged(R.id.switchImportance)
+    public void change(CompoundButton compoundButton, boolean isChecked) {
+        isHighImportance = isChecked;
+    }
+
+    private void sendNotification() {
+        Toast.makeText(this, "Working", Toast.LENGTH_SHORT).show();
     }
 }
